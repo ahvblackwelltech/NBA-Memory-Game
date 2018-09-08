fc22c0978927ead523e65dba5871a43915917ac3
Ahvi Blackwell <alblackwelldev@outlook.com>
2018-09-07 23:16:57 -0500

    Update script

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        // the first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // the second click 
+        hasFlippedCard = false;
+        secondCard = this;
+
+    }
 }
 cards.forEach(card => card.addEventListener('click', flipCard));
